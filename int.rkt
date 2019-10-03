b4dae09b0d6c5d36e0e99460a4939782d5cc2cec
sharkovadarya <sharkovadarya@gmail.com>
2019-10-03 20:05:52 +0300

    int fixes

--- int.rkt
+++ int.rkt
@@ -4,7 +4,7 @@
 (provide int tm-int)
 
 (define int
-  (lambda [program data]
+  (lambda (program data)
     (let ([prog (init-prog (cdr program))]
           [state (init-state (cdar program) data)])
       (int-basic-block prog state (cdadr program)))))
@@ -18,12 +18,6 @@
       [`(,head . ,tail)
        (int-basic-block program (int-assignment state head) tail)])))
 
-(define int-assignment
-  (lambda (state assignment)
-    (match assignment
-      [`(:= ,variable ,expression) (let ([new-value (eval-exp state expression)]) (state-set variable new-value state))]
-      [_ (error "no assignment found")])))
-
 (define int-jump
   (lambda (program state instruction)
     (match instruction
@@ -34,33 +28,39 @@
            (int-basic-block program state (dict-ref program label2)))]
       [`(return ,expression) (eval-exp state expression)])))
 
+(define int-assignment
+  (lambda (state assignment)
+    (match assignment
+      [`(:= ,variable ,expression) (let ([new-value (eval-exp state expression)]) (state-set state variable new-value))]
+      [_ (error "no assignment found")])))
+
 (define tm-int
-  '((read Q Right)
+  `((read Q Right)
     (init (:= Qtail Q) (:= Left '()) (goto loop))
     (loop (if (eq? Qtail '()) stop cont))
-    (cont (:= Instruction (cdar Qtail))
-          (:= Qtail (cdr Qtail))
-          (:= Operator (car Instruction))
-          (if (equal? Operator 'right) do-right cont1))
-    (cont1 (if (equal? Operator 'left) do-left cont2))
-    (cont2 (if (equal? Operator 'write) do-write cont3))
-    (cont3 (if (equal? Operator 'goto) do-goto cont4))
-    (cont4 (if (equal? Operator 'if) do-if error))
-    (do-right (:= Left (cons (car Right) Left))
-              (:= Right (cdr Right))
+    (cont  (:= Instruction (cdar Qtail))           
+           (:= Qtail (cdr Qtail))
+           (:= operator (car Instruction))
+           (if (equal? operator 'right) do-right cont1))
+    (cont1 (if (equal? operator 'left)  do-left  cont2))
+    (cont2 (if (equal? operator 'write) do-write cont3))
+    (cont3 (if (equal? operator 'goto)  do-goto  cont4))
+    (cont4 (if (equal? operator 'if)    do-if    error))
+    (do-right (:= Left (cons (safe-car Right) Left))
+              (:= Right (safe-cdr Right))
               (goto loop))
-    (do-left (:= Right (cons (car Left) Right))
-            (:= Left (cdr Left))
-            (goto loop))
+    (do-left  (:= Right (cons (safe-car Left) Right))
+              (:= Left (safe-cdr Left))
+              (goto loop))
     (do-write (:= Symbol (cadr Instruction))
               (:= Right (cons Symbol (cdr Right)))
               (goto loop))
-    (do-goto (:= Nextlabel (cadr Instruction))
-            (goto jump))
-    (do-if (:= Symbol (cadr Instruction))
-          (:= Nextlabel (cadddr Instruction))
-          (if (equal? Symbol (car Right)) jump loop))
+    (do-goto  (:= Nextlabel (cadr Instruction))
+              (goto jump))
+    (do-if    (:= Symbol (cadr Instruction))
+              (:= Nextlabel (cadddr Instruction))
+              (if (equal? Symbol (safe-car Right)) jump loop))
     (jump (:= Qtail (newtail Q Nextlabel))
           (goto loop))
-    (error (return ('unknown instruction ,Instruction)))
+    (error  (return `(unknown instruction ,Instruction)))
     (stop (return Right))))
